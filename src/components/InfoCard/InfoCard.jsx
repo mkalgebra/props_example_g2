77a3798239fe062d34d2a47e2d00a7d41df49c81
Marijo Kis <marijo.kis@q.agency>
2023-12-15 19:12:29 +0100

    chore: new components

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default class InfoCard extends React.Component {
-  render() {}
+  render() {
+    const { info } = this.props;
+    return (
+      <>
+        <p>
+          {info.label}: {info.value}
+        </p>
+      </>
+    );
+  }
 }
